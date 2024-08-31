830879d0a39cf6ff7495d11c05f7d8a9235603ef
sakhulbay <akhulbai8@gmail.com>
2024-08-31 15:40:50 +0500

    feat(): created client api

---
+++ src/main/java/kz/shyngys/client_api/ClientApiApplication.java
@@ -0,0 +1,13 @@
+package kz.shyngys.client_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApiApplication.class, args);
+    }
+
+}
